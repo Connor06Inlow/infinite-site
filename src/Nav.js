e254fd366877560da9722d90ccee0cfe149b4761
Connor <connor.sckans.edu>
2022-10-25 11:37:02 -0500

    initial commit

---
+++ src/Nav.js
@@ -0,0 +1,12 @@
+import React from "react";
+import './App.css';
+
+const Nav = () => {
+    return {
+        <div className="App"> 
+            sample 
+        </div>
+    }
+}
+
+export default App;
